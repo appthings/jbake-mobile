46ad026cad50c76bd85254024e13810ba05668d0
vic <vic@vics-MacBook-Pro.local>
2018-02-09 13:00:32 -0500

    no js, css or html

---
+++ www/assets/jsLibs/logger.ts
@@ -0,0 +1,42 @@
+declare var LogzioLogger: any
+ 
+(function(window) {
+
+    var LogzioLogger = function(apiKey, sendConsoleJsErrors) {
+        this.key = apiKey;
+        if (sendConsoleJsErrors) sendConsoleErrors();
+    };
+
+	var sendConsoleErrors = function() {
+		window.onerror = function (msg, url, line, col) {
+		    LogzioLogger.log({
+                        message: msg,
+                        url: url,
+                        line: line,
+                        col: col
+                    });
+		};
+	};
+    
+    LogzioLogger.prototype.log = function(data) {    
+        try {
+            var parsedMsg = typeof data == 'object' ? data : { message:data };
+            var logUrl = window.location.protocol + '//listener.logz.io:';
+            logUrl += (window.location.protocol === 'http:' ? '8090' : '8091') + '?token=' + this.key;
+
+            Object.keys(parsedMsg).forEach(function(key) {
+                logUrl += '&' + encodeURIComponent(key) + '=' + encodeURIComponent(parsedMsg[key]);
+            });
+
+            var logImg = new Image();
+            logImg.src = logUrl;
+        } catch (ex) {
+            if (window && window.console && typeof window.console.log == 'function') {
+                console.log("Failed to send log because of exception:\n" + ex);
+            }
+        }
+    };
+    
+    LogzioLogger = LogzioLogger;
+    
+})(window);
