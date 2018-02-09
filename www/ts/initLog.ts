8f49f196b979bb88a8405deca62872f38efbcdb2
vic <vic@vics-MacBook-Pro.local>
2018-02-09 17:33:54 -0500

    snap the logger

--- www/ts/initLog.ts
+++ www/ts/initLog.ts
@@ -1,1 +1,13 @@
 //type in your logz.io key here
+
+declare var Logger: any
+
+var token = 'uCWXDUBVYBqZfGBzYBHYoewoUnRQawYI'
+
+Logger = new LogzioLogger(token)
+
+Logger.log({
+	param1: 'value1',
+	param2: 'value2',
+	message: 'Oh hi'
+})
