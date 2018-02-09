2e2a9a78a2f302f6983788b43b4ae4d24f9cde70
vic <vic@vics-MacBook-Pro.local>
2018-02-09 14:19:29 -0500

    touched ?

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,19 @@ var gulp = require('gulp')
 var less = require('gulp-less')
 var LessAutoprefix = require('less-plugin-autoprefix')
 var autoprefix = new LessAutoprefix( { browsers: ['last 2 versions'] } )
+var debug = require('gulp-debug')
 
 var ts = require('gulp-typescript')
 var pug  = require('gulp-pug')
 var riot =  require('gulp-riot')
 var rename = require('gulp-rename')
-//var path = require('path')
 gulp.task('default', defaultTask)
 function defaultTask(done) {
 	gulp.start('css', 'html', 'comp', 'js')
 	done()
-	}
+}
 
-	//
+//
 gulp.task('watch', function(done) {
 	gulp.start('css', 'html', 'comp', 'js')
 	gulp.watch('./www/**/*.pug', ['comp', 'css', 'html', 'js'])
@@ -39,22 +39,27 @@ gulp.task('html', function() {
 		.pipe(gulp.dest('./www/'))
 	})
 
+
 gulp.task('comp', function() {  
 	return gulp.src('./www/comps/*.pug')
-		.pipe(pug(
-			{ pretty: true }
+		.pipe( pug(
+			{pretty: true }
 		)) // pipe to pug plugin
 		.pipe( rename({ extname: '.tag'}))
-		.pipe( riot())
+		.pipe( riot( 
+			{ } 
+		))
+		.pipe( rename({ extname: '.js'}))
+		//.pipe(debug())
 		.pipe(gulp.dest('./www/comps/'))
 	})
 
-gulp.task('tag', function() {// lets you see a tag before compile
+gulp.task('tag', function() {  
 	return gulp.src('./www/comps/*.pug')
 		.pipe( pug(
-			{ pretty: true }
+			{pretty: true }
 		)) // pipe to pug plugin
-		.pipe(rename({ extname: '.tag'}))
+		.pipe( rename({ extname: '.tag'}))
 		.pipe(gulp.dest('./www/comps/'))	
 	})
 
